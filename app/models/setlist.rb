41b7c86df2068fb371efcef4fef5eb94bc1b0409
Richard Whittaker <pkeruno@gmail.com>
2015-11-21 18:57:02 +0000

    Add basic validation

--- app/models/setlist.rb
+++ app/models/setlist.rb
@@ -1,2 +1,6 @@
 class Setlist < ActiveRecord::Base
+	validates :title, presence: true, length: { maximum: 255 }
+	validates :author, length: { maximum: 255 }
+	validates :genre, length: { maximum: 255 }
+	validates :edit_key, presence: true, length: { in: 5..6 } 
 end
--- spec/models/setlist_spec.rb
+++ spec/models/setlist_spec.rb
@@ -1,5 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe Setlist, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+	it { should validate_presence_of(:title) }
+	it { should validate_presence_of(:edit_key) }
+	it { should validate_length_of(:title).is_at_most(255) }
+	it { should validate_length_of(:author).is_at_most(255) }
+	it { should validate_length_of(:genre).is_at_most(255) }
+	it { should validate_length_of(:edit_key).is_at_least(5).is_at_most(6) }
 end
